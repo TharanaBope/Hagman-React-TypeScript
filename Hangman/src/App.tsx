241fe0d06c8fad37cccc823bb56c4343df7f44b0
Tharana Bopearachchi <tharanabope30@gmail.com>
2024-12-22 14:15:55 +0530

    completed the hangman Drawing

--- Hangman/src/App.tsx
+++ Hangman/src/App.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import words from './wordList.json'
+import { HangmanDrawing } from './HangmanDrawing'
+import { HangmanWord } from './HangmanWord'
+import { Keyboard } from './Keyboard'
 function App(){
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
