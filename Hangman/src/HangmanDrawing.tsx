02b97e21a90cec9d1b5354648a164e0c75a6ed42
Tharana Bopearachchi <tharanabope30@gmail.com>
2024-12-22 15:52:05 +0530

    completeed

--- Hangman/src/HangmanDrawing.tsx
+++ Hangman/src/HangmanDrawing.tsx
@@ -90,39 +90,38 @@ const HEAD = (
   type HangmanDrawingProps = {
     numberOfGuesses: number
   }
-export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps){
-    return(
-        <div style={{position: 'relative'}}>
-            {BODY_PARTS.slice(0, numberOfGuesses)}
-            <div 
-                style={{
-                    height: '50px', 
-                    width: '10px', 
-                    backgroundColor: 'black', 
-                    position: 'absolute',
-                    top: 0,
-                    right: 0,
-                }}
-            />
-            <div 
-                style={{
-                    height: '10px', 
-                    width: '200px', 
-                    backgroundColor: 'black', 
-                    marginLeft: '120px' }}
-            />
-            <div 
-                style={{
-                    height: '400px', 
-                    width: '10px', 
-                    backgroundColor: 'black', 
-                    marginLeft: '120px' }}
-            />
-            <div style={{
-                height: '10px', 
-                width: '250px', 
-                backgroundColor: 'black' }}
-                />
-        </div>
+  
+  export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
+    return (
+      <div style={{ position: "relative" }}>
+        {BODY_PARTS.slice(0, numberOfGuesses)}
+        <div
+          style={{
+            height: "50px",
+            width: "10px",
+            background: "black",
+            position: "absolute",
+            top: 0,
+            right: 0,
+          }}
+        />
+        <div
+          style={{
+            height: "10px",
+            width: "200px",
+            background: "black",
+            marginLeft: "120px",
+          }}
+        />
+        <div
+          style={{
+            height: "400px",
+            width: "10px",
+            background: "black",
+            marginLeft: "120px",
+          }}
+        />
+        <div style={{ height: "10px", width: "250px", background: "black" }} />
+      </div>
     )
-}
+  }
